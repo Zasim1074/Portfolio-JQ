1ceceda45788a26cc73294c21051c8a4d02ef866
Jaseem Quraishi <rdgvszasim@gmail.com>
2025-12-28 15:09:45 +0530

    Revise skill labels and set percentages to 100%
    Updated skill labels and percentages for frontend and backend skills.

--- src/containers/skills/utils.js
+++ src/containers/skills/utils.js
@@ -1,45 +1,73 @@
 export const skillData = [
   {
-    label: "Frontend",
+    label: "Frontend Web",
     Data: [
       {
         skillName: "HTML",
-        percentage: "95",
+        percentage: "100",
       },
       {
         skillName: "CSS",
-        percentage: "90",
+        percentage: "100",
       },
       {
         skillName: "Javascript",
-        percentage: "90",
+        percentage: "100",
       },
       {
         skillName: "ReactJS",
-        percentage: "90",
+        percentage: "100",
+      },
+      {
+        skillName: "Bootstrap",
+        percentage: "100",
+      },
+      {
+        skillName: "React Hooks",
+        percentage: "100",
+      },
+      {
+        skillName: "React Router",
+        percentage: "100",
+      },
+      {
+        skillName: "Rest APIs",
+        percentage: "100",
+      },
+      {
+        skillName: "Async Data Handling",
+        percentage: "100",
+      },
+      {
+        skillName: "Postman",
+        percentage: "100",
       },
     ],
   },
 
   {
-    label: "Backend",
+    label: "Frontend Mobile",
     Data: [
       {
-        skillName: "Java",
-        percentage: "80",
+        skillName: "Dart",
+        percentage: "100",
       },
       {
-        skillName: "C++",
-        percentage: "70",
+        skillName: "Flutter",
+        percentage: "100",
       },
       {
-        skillName: "Python",
-        percentage: "70",
+        skillName: "Responsive UI (Mobile & Tablet)",
+        percentage: "100",
       },
       {
-        skillName: "C",
-        percentage: "70",
+        skillName: "Cross-Platform Development",
+        percentage: "100",
       },
+      {
+        skillName: "WebSockets",
+        percentage: "100",
+      }
     ],
   },
 ];
